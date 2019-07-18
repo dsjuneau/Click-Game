ba7b609fb77ec497b50937156068bc689695b047
Scott Juneau <dsjuneau@gmail.com>
2019-07-18 14:55:02 -0500

    footer done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="pt-5 pb-1 container-fluid bg-dark text-white-50">
+      <p>Clicky Game!</p>
+    </div>
+  );
 }
 
 export default Footer;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Header() {
   return (
-    <div className="py-5 container-fluid bg-dark text-white-50">
+    <div className="py-5 container-fluid bg-dark text-white-50 sticky-top">
       <div className="row">
         <div className="text-center font-weight-bold h1 col-3">Clicky Game</div>
         <div className="text-center font-weight-light h2 col-4">Filler</div>
