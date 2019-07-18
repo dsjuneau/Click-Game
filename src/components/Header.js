a39677a3a5dc7710e1c251781e89a2804bd02e9f
Scott Juneau <dsjuneau@gmail.com>
2019-07-18 14:05:58 -0500

    header up

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="py-5 container-fluid bg-dark text-white-50">
+      <div className="row">
+        <div className="text-center font-weight-bold h1 col-3">Clicky Game</div>
+        <div className="text-center font-weight-light h2 col-4">Filler</div>
+        <div className="text-center font-weight-bold h1 col-2">Score</div>
+        <div className="text-center font-weight-bold h1 col-3">Top Score</div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
